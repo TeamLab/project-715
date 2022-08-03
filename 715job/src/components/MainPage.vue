d9989e79aa3d58a215446c2ba8bdb5b6d7c0f897
yongchoooon <sod7050@gmail.com>
2022-08-03 16:06:27 +0900

    new : vue 구조

---
+++ 715job/src/components/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center">
+    <h1 id="aaa">aaa</h1>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ 715job/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HelloWorld />
+    <MainPage />
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+import MainPage from '../components/MainPage'
+
+export default {
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+    MainPage
+  }
+}
+</script>
