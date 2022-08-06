000677ebddd43c65f08d424cda8ccd6fa4279324
buhyun <hwangbh8@naver.com>
2022-08-06 10:28:44 +0900

    chore:rsvinfo delete script

--- 715job/src/views/mypage/MyRsvInfoView.vue
+++ 715job/src/views/mypage/MyRsvInfoView.vue
@@ -42,14 +42,6 @@
 
 <script>
 export default {
-  data: () => ({
-    selectedItem: 1,
-    items: [
-      { text: 'Real-Time', icon: 'mdi-clock' },
-      { text: 'Audience', icon: 'mdi-account' },
-      { text: 'Conversions', icon: 'mdi-flag' }
-    ]
-  })
 }
 </script>
 <style scoped>
