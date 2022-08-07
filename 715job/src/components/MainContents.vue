1e0a8c86c0e9c7de60794846807101091b0af0dc
Mariposa818 <sympathy6027@naver.com>
2022-08-07 19:09:26 +0900

    MainContents

--- 715job/src/components/MainContents.vue
+++ 715job/src/components/MainContents.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <div>
-      <div v-for="i in 15" :key="i">
-        <h1>aaa</h1>
+      <div>
+        <h1 class="bold">PKNU SME 가온관 715호 사용규칙</h1>
+        <p class="a">팀 프로젝트 및 세미나 진행을 위한 협업룸으로 사용됩니다!</p>
+        <p class="a">함께 사용하고자 하는 공간이 필요한 학생이라면 누구나 예약 후 사용하실 수 있습니다.</p>
+        &nbsp;&nbsp;
+        <h2 class="bold">사용 규칙</h2>
+        &nbsp;
+        <p class="a">1. 예약 시에는 예약자가 누구인지 알 수 있도록 한다. </p>
+        <p class="a">2. 사용 가능한 시간 제약은 없지만, 사용하고자 하는 인원이 많을 경우에는 2~3시간 정도의 사용이 권장된다.</p>
+        <p class="a">3. 협업룸은 모두가 함께 사용하는 공간이니 서로를 위해 청결하고 소중하게 사용한다.</p>
       </div>
     </div>
   </div>
@@ -22,3 +30,11 @@ export default {
   methods: {}
 }
 </script>
+
+<style scoped>
+.a{
+  text-align: left;
+  font-size: 16px;
+  font-family: "Verdana";
+}
+</style>
