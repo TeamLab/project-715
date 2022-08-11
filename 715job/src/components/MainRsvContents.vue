67399a20027717987bc9a238fe8186f72ed70d43
Yonggeun Kwon <dydrms29@naver.com>
2022-08-12 00:07:23 +0900

    feat: 로고를 눌렀을 때 스크롤 올리는 기능 추가

--- 715job/src/components/MainRsvContents.vue
+++ 715job/src/components/MainRsvContents.vue
@@ -1,107 +1,149 @@
 <template>
-<div>
-  <div class="main-rsv-contents">
-    <div class="rsv-text">
-      <h1>
-        예약 시각 선택
-      </h1>
-    </div>
-    <div class="timecontents">
-      <ul class="timeLists">
-        <li v-for="i in 23" :key="i">
-          <v-btn
-            :id="'timetable-btn-'+i"
-            class="timetable"
-            elevation="0"
-            :ripple="false"
-            :color="colors[i]"
-            @click="pickTime(i)"
-            >{{ i }}
-          </v-btn>
-        </li>
-      </ul>
-    </div>
-    <div class="rsv-text">
-      <h1>
-        예약 정보 입력
-      </h1>
-    </div>
-    <div class="contents-row content-row-1">
-      <div class="content-box">
-        <span class="input-title"><span class="text-orangered">*</span> 예약 날짜</span>
-        <input
-        type="text"
-        id="selectedDate"
-        class="input-text-readonly"
-        :value="RsvDate"
-        readonly
-      />
+  <div>
+    <div class="main-rsv-contents">
+      <div class="rsv-text">
+        <h1>예약 시각 선택</h1>
       </div>
-      <div class="content-box">
-        <span class="input-title"><span class="text-orangered">*</span> 예약자</span>
-        <input class="input-text-readonly"  type="text" value="권용근" readonly />
+      <div class="timecontents">
+        <ul class="timeLists">
+          <li v-for="i in 23" :key="i">
+            <v-btn
+              :id="'timetable-btn-' + i"
+              class="timetable"
+              elevation="0"
+              :ripple="false"
+              :color="colors[i]"
+              @click="pickTime(i)"
+              >{{ i }}
+            </v-btn>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div class="contents-row content-row-2" >
-      <div class="content-box">
-        <span class="input-title"><span class="text-orangered">*</span> 이용시작시각</span>
-        <input class="input-text-readonly" type="text" :value="start_time+' : 00'" readonly />
+      <div class="rsv-text">
+        <h1>예약 정보 입력</h1>
       </div>
-      <div style="margin-top:auto;margin-bottom:auto;">
-        ~
+      <div class="contents-row content-row-1">
+        <div class="content-box">
+          <span class="input-title"
+            ><span class="text-orangered">*</span> 예약 날짜</span
+          >
+          <input
+            type="text"
+            id="selectedDate"
+            class="input-text-readonly"
+            :value="RsvDate"
+            readonly
+          />
+        </div>
+        <div class="content-box">
+          <span class="input-title"
+            ><span class="text-orangered">*</span> 예약자</span
+          >
+          <input
+            class="input-text-readonly"
+            type="text"
+            value="권용근"
+            readonly
+          />
+        </div>
       </div>
-      <div class="content-box">
-        <span class="input-title"><span class="text-orangered">*</span> 이용종료시각</span>
-        <input class="input-text-readonly" type="text" :value="display_end_time+' : 00'" readonly />
+      <div class="contents-row content-row-2">
+        <div class="content-box">
+          <span class="input-title"
+            ><span class="text-orangered">*</span> 이용시작시각</span
+          >
+          <input
+            class="input-text-readonly"
+            type="text"
+            :value="start_time + ' : 00'"
+            readonly
+          />
+        </div>
+        <div style="margin-top: auto; margin-bottom: auto">~</div>
+        <div class="content-box">
+          <span class="input-title"
+            ><span class="text-orangered">*</span> 이용종료시각</span
+          >
+          <input
+            class="input-text-readonly"
+            type="text"
+            :value="display_end_time + ' : 00'"
+            readonly
+          />
+        </div>
       </div>
-    </div>
-    <div class="contents-row content-row-3">
-      <div class="content-box">
-        <span class="input-title"><span class="text-orangered">*</span> 이용 인원</span>
-        <select class="peopleNum">
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-        </select>
+      <div class="contents-row content-row-3">
+        <div class="content-box">
+          <span class="input-title"
+            ><span class="text-orangered">*</span> 이용 인원</span
+          >
+          <select class="peopleNum">
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </div>
+        <div class="content-box">
+          <span class="input-title"
+            ><span class="text-orangered">*</span> 테이블 번호</span
+          >
+          <input
+            class="input-text-readonly"
+            type="text"
+            :value="'Table ' + RsvTable"
+            readonly
+          />
+        </div>
       </div>
-      <div class="content-box">
-        <span class="input-title"><span class="text-orangered">*</span> 테이블 번호</span>
-        <input class="input-text-readonly" type="text" :value="'Table '+RsvTable" readonly />
-      </div>
-    </div>
-    <div class="contents-row-last content-row-4">
+      <div class="contents-row-last content-row-4">
         <span class="input-title">&nbsp;&nbsp;예약 내용</span>
         <textarea cols="2"></textarea>
-    </div>
-    <div class="rsv-text">
-      <h1>유의사항</h1>
-    </div>
-    <div class="notice-contents">
-      <div style="margin-bottom:24px;">
-        <p>1. 715호 협업룸에서 이용한 물품 및 설비는 이용 후 반드시 제자리에 둔다.</p>
-        <p>2. 715호 협업룸을 함께 이용하는 모두를 위해 이용 이후 반드시 주변 청결을 유지한다.</p>
-        <p>3. 715호 협업룸을 같은 시각에 다른 팀과 함께 이용할 경우, 서로에게 방해가 되지 않도록 배려하며 이용한다.</p>
-        <p>4. 715호 협업룸 이용 예약자 및 이용자는 715호 협업룸의 설비와 기물을 고장 및 파손에 대해 유의하며 이용한다.</p>
+      </div>
+      <div class="rsv-text">
+        <h1>유의사항</h1>
+      </div>
+      <div class="notice-contents">
+        <div style="margin-bottom: 24px">
+          <p>
+            1. 715호 협업룸에서 이용한 물품 및 설비는 이용 후 반드시 제자리에
+            둔다.
+          </p>
+          <p>
+            2. 715호 협업룸을 함께 이용하는 모두를 위해 이용 이후 반드시 주변
+            청결을 유지한다.
+          </p>
+          <p>
+            3. 715호 협업룸을 같은 시각에 다른 팀과 함께 이용할 경우, 서로에게
+            방해가 되지 않도록 배려하며 이용한다.
+          </p>
+          <p>
+            4. 715호 협업룸 이용 예약자 및 이용자는 715호 협업룸의 설비와 기물을
+            고장 및 파손에 대해 유의하며 이용한다.
+          </p>
+        </div>
+      </div>
+      <div class="confirm-note">
+        <input
+          class="confirm-note-checkbox"
+          type="checkbox"
+          v-model="isChecked"
+        />
+        <div class="confirm-note-text">위의 사항에 동의합니다.</div>
+      </div>
+      <div class="rsv-button">
+        <router-link to="/rsvcomplete" style="text-decoration: none">
+          <v-btn
+            id="rsv-button"
+            :ripple="false"
+            color="#002448"
+            :disabled="!isChecked"
+          >
+            <span style="color: white">예약하기</span>
+          </v-btn>
+        </router-link>
       </div>
     </div>
-    <div class="confirm-note">
-      <input class="confirm-note-checkbox" type="checkbox" v-model="isChecked" />
-      <div class="confirm-note-text">위의 사항에 동의합니다.</div>
-    </div>
-    <div class="rsv-button">
-      <router-link to="/rsvcomplete" style="text-decoration:none;">
-        <v-btn
-          id="rsv-button"
-          :ripple="false"
-          color="#002448"
-          :disabled="!isChecked"
-        >
-          <span style="color:white;">예약하기</span>
-        </v-btn>
-      </router-link>
-    </div>
   </div>
-</div>
 </template>
 <script>
 export default {
@@ -156,30 +198,38 @@ export default {
     gray(i) {
       this.colors[i] = '#d9d9d9'
     },
-    setStartTime(i) { // 시작시각 설정
+    setStartTime(i) {
+      // 시작시각 설정
       this.start_time = i
     },
-    setEndTime(i) { // 종료시각 설정 및 화면에 표시되는 종료시각 설정
+    setEndTime(i) {
+      // 종료시각 설정 및 화면에 표시되는 종료시각 설정
       this.end_time = i
       this.display_end_time = Number(this.end_time) + 1 // 클릭은 버튼 14를 클릭하지만 종료시각은 15시00분이기 때문에 + 1 해줌
     },
     pickTime(i) {
       this.count += 1 // click 하면 count + 1
-      if (this.count % 2 === 1) { // count가 홀수면
+      if (this.count % 2 === 1) {
+        // count가 홀수면
         this.setStartTime(i) // 시작시각을 클릭한 시각으로 설정하고
-        for (let j = 1; j <= 23; j++) { // 클릭한 시각 이외는 전부 회색으로 해라
+        for (let j = 1; j <= 23; j++) {
+          // 클릭한 시각 이외는 전부 회색으로 해라
           this.gray(j)
         }
         this.blue(this.start_time) // 클릭한 시각은 파란색으로 해라
-      } else { // count가 짝수면
-        if (i < this.start_time) { // 근데 만약 클릭한 시각이 기존에 정해졌던 시작시각보다 이전의 시각이면
+      } else {
+        // count가 짝수면
+        if (i < this.start_time) {
+          // 근데 만약 클릭한 시각이 기존에 정해졌던 시작시각보다 이전의 시각이면
           this.count = this.count - 1 // count - 1
           this.gray(this.start_time) // 기존에 선택한 시작시각은 회색으로 하고
           this.setStartTime(i) // 클릭한 시각으로 시작시각을 새롭게 설정해라
           this.blue(this.start_time) // 그리고 클릭한 시각은 파란색으로 해라
-        } else { // 클릭한 시각이 기존에 정해졌던 시작시각보다 이후의 시각이면
+        } else {
+          // 클릭한 시각이 기존에 정해졌던 시작시각보다 이후의 시각이면
           this.setEndTime(i) // 그 시각을 종료시각으로 설정해라
-          for (let j = this.start_time; j <= this.end_time; j++) { // 그리고 시작시각부터 종료시각까지 전부 파란색으로 해라
+          for (let j = this.start_time; j <= this.end_time; j++) {
+            // 그리고 시작시각부터 종료시각까지 전부 파란색으로 해라
             this.blue(j)
           }
         }
@@ -241,7 +291,8 @@ li {
 .input-text-readonly {
   background-color: #efefef;
 }
-input, select {
+input,
+select {
   border: 1px solid #658ef8;
   border-radius: 10px;
   text-align: center;
@@ -303,8 +354,8 @@ option {
   margin: 4px 8px 4px 0px;
 }
 .confirm-note-text {
-  line-height:32px;
-  font-size:24px;
+  line-height: 32px;
+  font-size: 24px;
 }
 .rsv-button {
   margin-bottom: 48px;
--- 715job/src/views/HomeView.vue
+++ 715job/src/views/HomeView.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    <MainPage @setBooking="setInput"/>
-    <MainContents v-if="!isBooking"/>
+    <MainPage @setBooking="setInput" />
+    <MainContents v-if="!isBooking" />
     <div ref="MainCalendar">
-      <MainCalendar v-if="isBooking" @isDatePicked="showFloorPlan" :hideDummy="isDatePicked"/>
+      <MainCalendar
+        v-if="isBooking"
+        @isDatePicked="showFloorPlan"
+        :hideDummy="isDatePicked"
+      />
     </div>
     <div ref="MainFloorPlan">
-      <MainFloorPlan v-if="isDatePicked" @isTablePicked="showRsvContents" :hideDummy="isTablePicked"/>
+      <MainFloorPlan
+        v-if="isDatePicked"
+        @isTablePicked="showRsvContents"
+        :hideDummy="isTablePicked"
+      />
     </div>
     <div ref="MainRsvContents">
-      <MainRsvContents v-if="isTablePicked" :RsvDate="PickedDate" :RsvTable="PickedTable"/>
+      <MainRsvContents
+        v-if="isTablePicked"
+        :RsvDate="PickedDate"
+        :RsvTable="PickedTable"
+      />
     </div>
   </div>
 </template>
@@ -41,29 +53,36 @@ export default {
   },
   watch: {
     isTablePicked() {
-      window.scrollTo({ top: this.$refs.MainRsvContents.offsetTop + 96, behavior: 'smooth' })
+      window.scrollTo({
+        top: this.$refs.MainRsvContents.offsetTop + 96,
+        behavior: 'smooth'
+      })
     }
   },
   methods: {
     setInput(isBooking) {
       this.isBooking = isBooking
-      window.scrollTo({ top: this.$refs.MainCalendar.offsetTop, behavior: 'smooth' })
+      window.scrollTo({
+        top: this.$refs.MainCalendar.offsetTop,
+        behavior: 'smooth'
+      })
     },
     showFloorPlan(isDatePicked, PickedDate) {
       this.isDatePicked = isDatePicked
       this.PickedDate = PickedDate
-      window.scrollTo({ top: this.$refs.MainFloorPlan.offsetTop + 96, behavior: 'smooth' })
+      window.scrollTo({
+        top: this.$refs.MainFloorPlan.offsetTop + 96,
+        behavior: 'smooth'
+      })
     },
     showRsvContents(isTablePicked, PickedTable) {
       this.isTablePicked = isTablePicked
       this.PickedTable = PickedTable
-      window.scrollTo({ top: this.$refs.MainFloorPlan.offsetTop + 134, behavior: 'smooth' })
+      window.scrollTo({
+        top: this.$refs.MainFloorPlan.offsetTop + 134,
+        behavior: 'smooth'
+      })
     }
   }
 }
 </script>
-<style>
-.dummy-calendar {
-  display: none;
-}
-</style>
